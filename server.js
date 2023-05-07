790dcb55bd0c5bc020a81aba1d57247375051dcc
Sampo30 <koskela.sampo@gmail.com>
2023-05-07 10:54:57 +0300

    mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
 
 //routes
 
@@ -7,6 +9,19 @@ app.get('/', (reg, res) => {
     res.send('Hello Sports API')
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API is running on port 3000')
+app.get('/blog', (reg, res) => {
+    res.send('Hello Blog')
+})
+
+
+
+mongoose.
+connect('mongodb+srv://admin:1234admin@sampocluster.cmcmgou.mongodb.net/SportsAPI?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API is running on port 3000')
+    });
+}).catch((error) => {
+    console.log(error)
 })
